7687eac18f03177a97a25b653b806fb1ebd9a3ec
mltomacheski <160253044+mltomacheski@users.noreply.github.com>
2025-04-24 10:52:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -24,13 +24,29 @@ capitais.push("Manaus");
 // adicionar valor no inicio do vetor e retorna o novo tamanho do vetor 
 console.log(capitais.unshift("Curitiba")); 
 
-
 //Remove o elemento do inicio do vetor. Retorna o elemento removido 
 capitais.shift();
+
+//Remove o ultimo elemento do vetor 
+capitais.pop();
+
+//Alterar o conteúdo do vetor escolhido,remover o elemento(s), posição inicial 
+//Elemento a ser inserido 
+//O 0 é o indice, o 1 é a qtd de elementos e o novo elemento "toquio"
+capitais.splice(0, 1, "Tokyo");
 
 for (let x = 0; x < capitais.length; x++ ){
      console.log (capitais [x]);// imprimir a posição do vetor 
  }
+
+console.log("======================================================")
+
+
+//Retornar um intervalo do vetor
+//Posição inicial e Posição final
+//obs: não altera o vetor original
+console.log(capitais.slice(0, 2));
+
 
 
 
